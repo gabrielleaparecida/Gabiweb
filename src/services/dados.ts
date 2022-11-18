7619c093cc0e3373e5a40405a88738ecb93df23c
gabrielleaparecida <ferreiragabrielle817@gmail.com>
2022-11-18 08:11:31 -0300

    mensagem

--- src/services/dados.ts
+++ src/services/dados.ts
@@ -1,6 +1,137 @@
 const dados = [
- 
-
-]
-
-export default dados
+    {
+      id: 1,
+      nome: "Gabrielle",
+      data: "24/09/2022 07:29:00",
+      titulo: "Colar",
+      mensagem: "Combina com o item?",
+      topico: [{
+        id: 1,
+        item: "Looks"
+      }, {
+        id: 2,
+        item: "Cor"
+      }]
+    },
+    {
+      id: 2,
+      nome: "Duda",
+      data: "10/10/2022 12:00:00",
+      titulo: "Calça",
+      mensagem: "Combina com a blusa rosa ?",
+      topico: [{
+        id: 1,
+        item: "Looks"
+      }, {
+        id: 2,
+        item: "Cor"
+      }, {
+        id: 3,
+        item: "Opção"
+      }]
+    },
+    {
+      id: 3,
+      nome: "Luana",
+      data: "28/04/2022 10:08:00",
+      titulo: "Calça",
+      mensagem: "Combina com a blusa rosa?",
+      topico: [{
+        id: 1,
+        item: "Looks"
+      }, {
+        id: 2,
+        item: "Cor"
+      }, {
+        id: 3,
+        item: "Opção"
+      }]
+    },
+    {
+      id: 4,
+      nome: "Samid",
+      data: "12/05/2022 22:08:00",
+      titulo: "Calça",
+      mensagem: "Combina com a blusa rosa?",
+      topico: [{
+        id: 1,
+        item: "Looks"
+      }, {
+        id: 2,
+        item: "Cor"
+      }, {
+        id: 3,
+        item: "Opção"
+      }]
+    },
+    {
+      id: 5,
+      nome: "Mary",
+      data: "01/10/2022 09:10:00",
+      titulo: "Calça",
+      mensagem: "Combina com a blusa rosa?",
+      topico: [{
+        id: 1,
+        item: "Looks"
+      }, {
+        id: 2,
+        item: "Cor"
+      }, {
+        id: 3,
+        item: "Opção"
+      }]
+    },
+    {
+      id: 6,
+      nome: "Julya",
+      data: "05/09/2022 14:56:00",
+      titulo: "Calça",
+      mensagem: "Combina com a blusa rosa ?",
+      topico: [{
+        id: 1,
+        item: "Looks"
+      }, {
+        id: 2,
+        item: "Cor"
+      }, {
+        id: 3,
+        item: "Opção"
+      }]
+    },
+    {
+      id: 7,
+      nome: "Andressa",
+      data: "14/05/2022 15:30:00",
+      titulo: "Calça",
+      mensagem: "Combina com a blusa rosa?",
+      topico: [{
+        id: 1,
+        item: "Looks"
+      }, {
+        id: 2,
+        item: "Cor"
+      }, {
+        id: 3,
+        item: "Opção"
+      }]
+    },
+    {
+      id: 8,
+      nome: "Livya",
+      data: "27/10/2022 16:05:00",
+      titulo: "Calça",
+      mensagem: "Combina com a blusa rosa?",
+      topico: [{
+        id: 1,
+        item: "Looks"
+      }, {
+        id: 2,
+        item: "Cor"
+      }, {
+        id: 3,
+        item: "Opção"
+      }]
+    },
+  ]
+  
+  export default dados
